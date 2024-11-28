5af63f27322103a1a09c94b388e977ca2a89861b
GabrielVilhenaMagri <gabriel.vilhenamagri@gmail.com>
2024-11-28 20:15:00 -0300

    Configuração do Banco de dados e da Tabela Veiculo

---
+++ mf-dev-backend-2024/Models/Veiculo.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_dev_backend_2024.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatorio")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatorio")]
+        public string Placa { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatorio")]
+        public int AnoFabricacao { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatorio")]
+        public int AnoModelo { get; set; }
+    }
+}
